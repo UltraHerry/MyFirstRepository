cd7ebfec931bf4d4db02204e9b1102465060c117
忘川Ultimate <ultimate_zhl@126.com>
2022-04-21 14:49:11 +0800

    初次上传

---
+++ src/main/java/com/ultra/text/domain/Student.java
@@ -0,0 +1,21 @@
+package com.ultra.text.domain;
+
+/**
+ * packageName: com.ultra.text.domain
+ * className: Student
+ * description:
+ *
+ * @Author: UltraHerry
+ * @Date: 2022/4/21 14:32
+ */
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/ultra/text/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultra.text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
